5c93733fba565574b4b9160e6602f03c06efc553
sarapappila <sara.pappila@metropolia.fi>
2023-08-21 10:59:43 +0300

    Uusi

---
+++ Tehtävät/tehtävä 1.py
@@ -0,0 +1,23 @@
+import math
+
+# tehtävä 1
+
+# 1a
+print(math.degrees(2.493))
+# 1b
+print(math.degrees(0.911))
+
+# 2a
+print(math.radians(137.7))
+# 2b
+print(math.radians(62.3))
+
+# 3
+print(math.radians(30))
+print(math.radians(45))
+print(math.radians(60))
+
+# tehtävä 2
+
+print(math.hypot(2.3, 4.7))
+
