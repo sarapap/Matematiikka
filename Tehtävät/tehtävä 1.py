b25e0ae7fe7ab1abf08b8b39b2506c005d9c332a
sarapappila <sara.pappila@metropolia.fi>
2023-08-21 13:05:18 +0300

    Tehty

--- Tehtävät/tehtävä 1.py
+++ Tehtävät/tehtävä 1.py
@@ -1,23 +1,42 @@
 import math
+from tabulate import tabulate
 
 # tehtävä 1
 
 # 1a
-print(math.degrees(2.493))
+print(f"Muunna asteiksi: {int(math.degrees(2.493))}")
 # 1b
-print(math.degrees(0.911))
+print(f"Muunna asteiksi: {int(math.degrees(0.911))}")
 
 # 2a
-print(math.radians(137.7))
+print(f"Muunna radiaaneiksi: {(math.radians(137.7)):.3f}")
 # 2b
-print(math.radians(62.3))
+print(f"Muunna radiaaneiksi: {(math.radians(62.3)):.3f}")
 
 # 3
-print(math.radians(30))
-print(math.radians(45))
-print(math.radians(60))
+table = [['Asteet', 'Radiaanit'],
+         ['30', round(math.radians(30), 3)],
+         ['45', round(math.radians(45), 3)],
+         ['60', round(math.radians(60), 3)],
+         ['90', round(math.radians(90), 3)],
+         ['120', round(math.radians(120), 3)],
+         ['135', round(math.radians(135), 3)],
+         ['150', round(math.radians(150), 3)],
+         ['180', round(math.radians(180), 3)],
+         ['270', round(math.radians(270), 3)],
+         ['360', round(math.radians(360), 3)]]
+
+print(tabulate(table))
 
 # tehtävä 2
 
-print(math.hypot(2.3, 4.7))
+print(f"Hypotenuusan pituus: {(math.hypot(2.3, 4.7)):.3f}")
+print(f"Kolmion toinen kulma: {math.degrees((math.acos(2.3 / 5.233))):.2f} astetta.")
+print(f"Kolmion kolmas kulma: {180 - 90 - 63.3:.2f} astetta.")
 
+# tehtävä 3
+
+print(f"Luvun 0 kertoma: {(math.factorial(0))}")
+print(f"Luvun 4 kertoma: {(math.factorial(4))}")
+print(f"Luvun 7 kertoma: {(math.factorial(7))}")
+print(f"Luvun 15 kertoma: {(math.factorial(15))}")
