221b80a4c69a1dff80e7a8aca1a794f6f262b6e6
sarapappila <sara.pappila@metropolia.fi>
2023-08-28 15:17:34 +0300

    Tehty

---
+++ Tehtävät/tehtävä 2.py
@@ -0,0 +1,17 @@
+import numpy as np
+import linalg
+from linalg import Matrix
+
+# 1. Määrittele ja esitä 5-alkioinen kokonaislukutaulukko numpyn avulla. luvut voivat olla satunnaisia.
+a = np.array([1, 2, 3, 4, 5])
+print(a)
+
+# 2. Sinulla on vektorit a) u = 2i + 3j, v =4i - 7j
+# b) uu= i + j + k, vv 3i -3j + 2k.
+# Määrittele nämä numpy taulukon avulla
+
+
+
+
+# 3. Laske kunkin vektorin normi.
+
